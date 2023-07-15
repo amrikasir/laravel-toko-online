a25ebc437e8d8c54148552afe5654f30a4e73789
Al Amrikasir <amrikasir@gmail.com>
2023-07-15 11:28:39 +0700

    fixed error route

--- routes/web.php
+++ routes/web.php
@@ -84,6 +84,3 @@ Route::group(['middleware' => ['auth','checkRole:customer']],function(){
     Route::get('/order/pembayaran/{id}','user\OrderController@pembayaran')->name('user.order.pembayaran');
     Route::post('/order/kirimbukti/{id}','user\OrderController@kirimbukti')->name('user.order.kirimbukti');
 });
-
-Route::get('/ongkir', 'OngkirController@index');
-Route::get('/ongkir/province/{id}/cities', 'OngkirController@getCities');
